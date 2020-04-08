e0cdd00eef208647943496521c85506bb3c2f7c2
Anežka Batěčková <anezka.bateckova@quanti.cz>
2020-04-08 02:57:00 +0200

    FEAT: Login button animation added

--- src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/main/authorization/fragment/LoginFragment.kt
+++ src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/main/authorization/fragment/LoginFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -14,6 +16,7 @@ import cz.quanti.android.vendor_app.main.authorization.viewmodel.LoginViewModel
 import cz.quanti.android.vendor_app.utils.CurrentVendor
 import cz.quanti.android.vendor_app.utils.VendorAppException
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_login.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -22,6 +25,7 @@ import quanti.com.kotlinlog.Log
 class LoginFragment : Fragment() {
 
     private val vm: LoginViewModel by viewModel()
+    private var disposable: Disposable? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,21 +51,38 @@ class LoginFragment : Fragment() {
             loginButton.isEnabled = true
             loginButton.setOnClickListener {
                 loginButton.isEnabled = false
+                loginButton.visibility = View.INVISIBLE
+                loadingImageView.visibility = View.VISIBLE
 
-                vm.login(usernameEditText.text.toString(), passwordEditText.text.toString())
+                val animation = RotateAnimation(
+                    0f,
+                    360f,
+                    loadingImageView.width / 2f,
+                    loadingImageView.height / 2f
+                )
+                animation.duration = 2500
+                animation.repeatCount = Animation.INFINITE
+                loadingImageView.startAnimation(animation)
+
+                disposable =
+                    vm.login(usernameEditText.text.toString(), passwordEditText.text.toString())
                     .subscribeOn(
                         Schedulers.io()
                     ).observeOn(AndroidSchedulers.mainThread()).subscribe(
                         {
+                            loadingImageView.animation.repeatCount = 0
                             findNavController().navigate(
                                 LoginFragmentDirections.actionLoginFragmentToVendorFragment()
                             )
                         },
                         {
+                            loadingImageView.clearAnimation()
+                            loadingImageView.visibility = View.INVISIBLE
+                            loginButton.visibility = View.VISIBLE
+                            loginButton.isEnabled = true
                             Log.e(it) // TODO the error might be caused by internet connection or other causes
                             usernameEditText.error = getString(R.string.wrong_password)
                             passwordEditText.error = getString(R.string.wrong_password)
-                            loginButton.isEnabled = true
                             //TODO just for debugging purposes
                             try {
                                 Toast.makeText(
@@ -81,5 +102,10 @@ class LoginFragment : Fragment() {
                     )
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
     }
 }

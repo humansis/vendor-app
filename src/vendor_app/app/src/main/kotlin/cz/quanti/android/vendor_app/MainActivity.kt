8103f3ee6c9792c3e75108b906ce797e7df906a1
Anežka Batěčková <anezka.bateckova@quanti.cz>
2020-07-13 15:34:01 +0200

    FIX: PIN-208 New Vendor App Multi Bug Report (action bar changes)

--- src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/MainActivity.kt
+++ src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/MainActivity.kt
@@ -5,12 +5,15 @@ import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.navigation.findNavController
 import cz.quanti.android.vendor_app.main.vendor.callback.VendorFragmentCallback
 import cz.quanti.android.vendor_app.repository.AppPreferences
@@ -37,31 +40,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        supportActionBar?.setCustomView(R.layout.custom_action_bar)
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.action_bar_actions, menu)
-        menu?.findItem(R.id.logoutButton)?.setOnMenuItemClickListener {
-            AlertDialog.Builder(this, R.style.DialogTheme)
-                .setTitle(getString(R.string.are_you_sure_dialog_title))
-                .setMessage(getString(R.string.logout_dialog_message))
-                .setPositiveButton(
-                    android.R.string.yes
-                ) { _, _ ->
-                    loginFacade.logout()
-                    findNavController(R.id.main_nav_host).popBackStack(R.id.loginFragment, false)
-                }
-                .setNegativeButton(android.R.string.no, null)
-                .show()
-
-            true
+        val leftMenu: View? = findViewById(R.id.left_menu)
+        leftMenu?.setOnClickListener {
+            showPopupMenu(it)
         }
 
-        val syncButton = menu?.findItem(R.id.syncButton)?.actionView as ImageView
-        syncButton.setImageResource(R.drawable.sync)
-        syncButton.setOnClickListener { view ->
+        val syncButton: View? = findViewById(R.id.syncButton) as ImageView
+        syncButton?.setOnClickListener { view ->
             val animation = RotateAnimation(0f, 360f, view.width / 2f, view.height / 2f)
             animation.duration = 2500
             animation.repeatCount = Animation.INFINITE
@@ -111,5 +102,39 @@ class MainActivity : AppCompatActivity() {
             val tag: Tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
             nfcTagPublisher.getTagSubject().onNext(tag)
         }
+    }
+
+    private fun showPopupMenu(v: View) {
+        PopupMenu(this, v).apply {
+            setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
+                override fun onMenuItemClick(item: MenuItem?): Boolean {
+                    return when (item?.itemId) {
+
+                        R.id.logoutButton -> {
+                            logout()
+                            true
+                        }
+                        else -> false
+                    }
+                }
+
+            })
+            inflate(R.menu.action_bar_left_actions)
+            show()
+        }
+    }
+
+    private fun logout() {
+        AlertDialog.Builder(this, R.style.DialogTheme)
+            .setTitle(getString(R.string.are_you_sure_dialog_title))
+            .setMessage(getString(R.string.logout_dialog_message))
+            .setPositiveButton(
+                android.R.string.yes
+            ) { _, _ ->
+                loginFacade.logout()
+                findNavController(R.id.main_nav_host).popBackStack(R.id.loginFragment, false)
+            }
+            .setNegativeButton(android.R.string.no, null)
+            .show()
     }
 }

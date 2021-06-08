8b7075be7df6a8a8c948faac1f2b8bb232d73763
neubauerm <20091994aB>
2021-06-08 16:16:27 +0200

    FEAT: new product layout

--- src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/main/vendor/viewholder/ShopViewHolder.kt
+++ src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/main/vendor/viewholder/ShopViewHolder.kt
@@ -1,20 +1,21 @@
 package cz.quanti.android.vendor_app.main.vendor.viewholder
 
 import android.view.View
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_product.view.*
 import kotlinx.android.synthetic.main.item_shop.view.*
 
 class ShopViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    var firstProductName: TextView? = itemView.firstProductTitle
-    var firstProductImage: LinearLayout? = itemView.firstProductImage
-    var firstProductLayout: RelativeLayout? = itemView.firstProductLayout
-    var secondProductName: TextView? = itemView.secondProductTitle
-    var secondProductImage: LinearLayout? = itemView.secondProductImage
-    var secondProductLayout: RelativeLayout? = itemView.secondProductLayout
-    var thirdProductName: TextView? = itemView.thirdProductTitle
-    var thirdProductImage: LinearLayout? = itemView.thirdProductImage
-    var thirdProductLayout: RelativeLayout? = itemView.thirdProductLayout
+    var firstProductName: TextView? = itemView.firstProduct.productName
+    var firstProductImage: ImageView? = itemView.firstProduct.productImage
+    var firstProductLayout: CardView? = itemView.firstProduct.productLayout
+    var secondProductName: TextView? = itemView.secondProduct.productName
+    var secondProductImage: ImageView? = itemView.secondProduct.productImage
+    var secondProductLayout: CardView? = itemView.secondProduct.productLayout
+    var thirdProductName: TextView? = itemView.thirdProduct.productName
+    var thirdProductImage: ImageView? = itemView.thirdProduct.productImage
+    var thirdProductLayout: CardView? = itemView.thirdProduct.productLayout
 }

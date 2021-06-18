166aec5584122123b37c9c1802aca80e305ca2fb
neubauerm <20091994aB>
2021-06-18 11:07:00 +0200

    FIX: empty purchases from db properly

--- src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/repository/purchase/impl/PurchaseRepositoryImpl.kt
+++ src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/repository/purchase/impl/PurchaseRepositoryImpl.kt
@@ -245,15 +245,28 @@ class PurchaseRepositoryImpl(
     }
 
     override fun deleteCardPurchase(purchase: Purchase): Completable {
-        return Completable.fromCallable { cardPurchaseDao.deleteCardForPurchase(purchase.dbId) }
+        return Completable.fromCallable { purchaseDao.delete(convertToDb(purchase)) }
+            .andThen( Completable.fromCallable {
+                cardPurchaseDao.deleteCardForPurchase(purchase.dbId)
+            })
     }
 
     override fun deleteVoucherPurchase(purchase: Purchase): Completable {
-        return Completable.fromCallable { voucherPurchaseDao.deleteVoucherForPurchase(purchase.dbId) }
+        return Completable.fromCallable { purchaseDao.delete(convertToDb(purchase)) }
+            .andThen( Completable.fromCallable {
+                voucherPurchaseDao.deleteVoucherForPurchase(purchase.dbId)
+            })
     }
 
     override fun deleteAllVoucherPurchases(): Completable {
-        return Completable.fromCallable{ voucherPurchaseDao.deleteAll() }
+        return getAllPurchases().flatMapCompletable { purchases ->
+            Observable.fromIterable(purchases.filter { it.vouchers.isNotEmpty()})
+                .flatMapCompletable {
+                    Completable.fromCallable {
+                        purchaseDao.delete(convertToDb(it))
+                    }
+                }
+        }.doOnComplete { (voucherPurchaseDao.deleteAll()) }
     }
 
     override fun getPurchasesCount(): Single<Int> {

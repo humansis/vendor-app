20cd6e9d47649669c2918a080a5c9bcbb4748c9a
Michal Neubauer <michal.neubauer@quanti.cz>
2021-12-13 16:16:16 +0100

    FEAT: delete old logs on successful upload

--- src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/repository/log/impl/LogFacadeImpl.kt
+++ src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/repository/log/impl/LogFacadeImpl.kt
@@ -7,6 +7,7 @@ import cz.quanti.android.vendor_app.utils.VendorAppException
 import cz.quanti.android.vendor_app.utils.isPositiveResponseHttpCode
 import io.reactivex.Completable
 import io.reactivex.Observable
+import quanti.com.kotlinlog.file.FileLogger
 import quanti.com.kotlinlog.utils.getZipOfLogs
 
 class LogFacadeImpl(
@@ -23,7 +24,7 @@ class LogFacadeImpl(
         return logRepo.postLogs(vendorId, getZipOfLogs(context, 48))
             .flatMapCompletable { responseCode ->
                 if (isPositiveResponseHttpCode(responseCode)) {
-                    Completable.complete()
+                    Completable.fromCallable { FileLogger.deleteAllLogs(context) }
                 } else {
                     throw VendorAppException("Could not upload Logs").apply {
                         this.apiResponseCode = responseCode

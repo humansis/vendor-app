c30f5a3737c27ccded8721342c77159eecf5da56
Michal Neubauer <michal.neubauer@quanti.cz>
2021-12-29 16:47:56 +0100

    REF: remove unused code

--- src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/repository/login/dto/api/LocationAdm3.kt
+++
@@ -1,8 +0,0 @@
-package cz.quanti.android.vendor_app.repository.login.dto.api
-
-data class LocationAdm3(
-    var id: Long = 0,
-    var name: String = "",
-    var adm2: LocationAdm2? = null,
-    var code: String = ""
-)
--- src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/repository/login/dto/api/LocationAdm4.kt
+++
@@ -1,8 +0,0 @@
-package cz.quanti.android.vendor_app.repository.login.dto.api
-
-data class LocationAdm4(
-    var id: Long = 0,
-    var name: String = "",
-    var adm3: LocationAdm3? = null,
-    var code: String = ""
-)

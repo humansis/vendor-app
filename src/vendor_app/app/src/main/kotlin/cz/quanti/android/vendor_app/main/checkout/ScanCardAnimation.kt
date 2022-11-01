0e8e982627188a80d4f41a393c6b7b00278f7ea3
neubauerm <michal.neubauer@quanti.cz>
2022-11-01 21:55:55 +0100

    REF: simplify scan card animation

--- src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/main/checkout/ScanCardAnimation.kt
+++ src/vendor_app/app/src/main/kotlin/cz/quanti/android/vendor_app/main/checkout/ScanCardAnimation.kt
@@ -14,86 +14,7 @@ import cz.quanti.android.vendor_app.databinding.LayoutScanCardAnimationBinding
 class ScanCardAnimation(
     private val scanCardBinding: LayoutScanCardAnimationBinding
 ) {
-    private var regularAnimation = ObjectAnimator.ofFloat(scanCardBinding.card, "translationX", -180f)
-        .apply {
-            var cardAttached = true
-
-            duration = 2000
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.REVERSE
-            doOnStart {
-                cardAttached = true
-            }
-            doOnPause {
-                if (cardAttached) {
-                    scanCardBinding.spinner.visibility = View.VISIBLE
-                }
-            }
-            doOnResume {
-                if (cardAttached) {
-                    scanCardBinding.spinner.visibility = View.INVISIBLE
-                    scanCardBinding.check.visibility = View.VISIBLE
-                } else {
-                    scanCardBinding.check.visibility = View.INVISIBLE
-                }
-            }
-            doOnRepeat {
-                pause()
-                Handler(Looper.getMainLooper()).postDelayed(
-                    {
-                        resume()
-                        cardAttached = !cardAttached
-                    },
-                    if (cardAttached) 1500 else 500
-                )
-            }
-            doOnEnd {
-                scanCardBinding.card.clearAnimation()
-                scanCardBinding.card.animate().translationX(0F).translationY(0F).setDuration(0).start()
-            }
-        }
-
-    private var preserveBalanceAnimation = ObjectAnimator.ofFloat(scanCardBinding.card, "translationX", -180f)
-        .apply {
-            var cardAttached = true
-
-            duration = 2000
-            repeatCount = ObjectAnimator.INFINITE
-            repeatMode = ObjectAnimator.REVERSE
-            doOnStart {
-                cardAttached = true
-                scanCardBinding.cross.visibility = View.VISIBLE
-            }
-            doOnPause {
-                if (cardAttached) {
-                    scanCardBinding.cross.visibility = View.INVISIBLE
-                    scanCardBinding.spinner.visibility = View.VISIBLE
-                }
-            }
-            doOnResume {
-                if (cardAttached) {
-                    scanCardBinding.spinner.visibility = View.INVISIBLE
-                    scanCardBinding.check.visibility = View.VISIBLE
-                } else {
-                    scanCardBinding.check.visibility = View.INVISIBLE
-                    scanCardBinding.cross.visibility = View.VISIBLE
-                }
-            }
-            doOnRepeat {
-                pause()
-                Handler(Looper.getMainLooper()).postDelayed(
-                    {
-                        resume()
-                        cardAttached = !cardAttached
-                    },
-                    if (cardAttached) 1500 else 500
-                )
-            }
-            doOnEnd {
-                scanCardBinding.card.clearAnimation()
-                scanCardBinding.card.animate().translationX(0F).translationY(0F).setDuration(0).start()
-            }
-        }
+    private var animation: ObjectAnimator? = null
 
     fun startScanCardAnimation(preserveBalance: Boolean) {
         scanCardBinding.root.visibility = View.VISIBLE
@@ -102,16 +23,56 @@ class ScanCardAnimation(
         scanCardBinding.spinner.visibility = View.INVISIBLE
         scanCardBinding.check.visibility = View.INVISIBLE
 
-        if (preserveBalance) {
-            preserveBalanceAnimation.start()
-        } else {
-            regularAnimation.start()
-        }
+        animation = ObjectAnimator.ofFloat(scanCardBinding.card, "translationX", -180f)
+            .apply {
+                var cardAttached = true
+
+                duration = 2000
+                repeatCount = ObjectAnimator.INFINITE
+                repeatMode = ObjectAnimator.REVERSE
+                doOnStart {
+                    cardAttached = true
+                    if (preserveBalance) scanCardBinding.cross.visibility = View.VISIBLE
+                }
+                doOnPause {
+                    if (cardAttached) {
+                        scanCardBinding.spinner.visibility = View.VISIBLE
+                        if (preserveBalance) scanCardBinding.cross.visibility = View.INVISIBLE
+                    }
+                }
+                doOnResume {
+                    if (cardAttached) {
+                        scanCardBinding.spinner.visibility = View.INVISIBLE
+                        scanCardBinding.check.visibility = View.VISIBLE
+                    } else {
+                        scanCardBinding.check.visibility = View.INVISIBLE
+                        if (preserveBalance) scanCardBinding.cross.visibility = View.VISIBLE
+                    }
+                }
+                doOnRepeat {
+                    pause()
+                    Handler(Looper.getMainLooper()).postDelayed(
+                        {
+                            resume()
+                            cardAttached = !cardAttached
+                        },
+                        if (cardAttached) 1500 else 500
+                    )
+                }
+                doOnEnd {
+                    scanCardBinding.card.clearAnimation()
+                    scanCardBinding.card.animate()
+                        .translationX(0F)
+                        .translationY(0F)
+                        .setDuration(0)
+                        .start()
+                }
+                start()
+            }
     }
 
     fun stopScanCardAnimation() {
-        regularAnimation.end()
-        preserveBalanceAnimation.end()
+        animation?.end()
         scanCardBinding.root.visibility = View.GONE
     }
 }
